6a05d1c70eefe6d42ae81273ce312cf2f71302ec
Aliaksei Kernoha <halflife.kernoga@yandex.by>
2020-08-24 22:03:46 +0300

    Added i18n as multilanguage tool

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
 import React from "react";
-
+import { useTranslation } from 'react-i18next';
 import '../styles/App.scss';
 
 import Prallaps from "./Prallaps"
 
 function App() {
+  const { t, i18n } = useTranslation();
+  const changeLanguage = lng => {
+    i18n.changeLanguage(lng);
+  };
   return (
     <div className="App">
       <h1>My React App!</h1>
-      <small>Подзаголовок</small>
+      <small>Something</small>
       <Prallaps />
+      <p>{t('paragraph')}</p>
+        <button className="btn" onClick={() => changeLanguage('en')}>english</button>
+        <button className="btn" onClick={() => changeLanguage('hn')}>hindi</button>
+        <button className="btn" onClick={() => changeLanguage('sp')}>spanish</button>
+        <button className="btn" onClick={() => changeLanguage('fr')}>french</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,24 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App.js";
-import * as serviceWorker from './serviceWorker';
-
-import {IntlProvider} from "react-intl";
-import locale_en from "./translations/en.json";
-import locale_ru from "./translations/ru.json";
-
-const data = {
-  'ru': locale_ru,
-  'en': locale_en
-};
-
-const language = navigator.language.split(/[-_]/)[0];
+import "./i18n";
 
 ReactDOM.render(
-  <IntlProvider locale={language} messages={data[language]}>
-    <App />
-  </IntlProvider>, 
+    <App />,
   document.getElementById("root")
 );
-
-serviceWorker.unregister();
