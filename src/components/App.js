7e87d4df30b4fbee117c2c5dd93d64f95b0d051c
Aliaksei Kernoha <halflife.kernoga@yandex.by>
2020-09-22 18:19:20 +0300

    Added infrostructure for jest tests

--- src/components/App.js
+++ src/components/App.js
@@ -21,7 +21,6 @@ export default function App() {
         <SimpleChatBot />
         <Footer />
       </div>
-
     </div>
   );
 }
---
+++ src/tests/App.test.js
@@ -0,0 +1,5 @@
+const App = require("../components/App");
+
+test('first test', () => {
+  expect(App.hasClass("App").to.equal(true))
+})
